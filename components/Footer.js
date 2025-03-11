60abc1dab101a78f0b2379911ccd3e520ebc4ce3
senotron <105975100+senotron@users.noreply.github.com>
2025-03-11 21:23:18 +0400

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Word Games.</p>
+      <p>@senotron</p>
+    </footer>
+  );
+}
